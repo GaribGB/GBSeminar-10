696021231c9bb21a9fb6fd49b89831fbbceda03c
Garib <garibabbasov@yandex.ru>
2022-07-28 23:11:55 +0700

    add GBSeminar 10

---
+++ GB73/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+ulong n = Input("Введите число N: ");
+ulong[] arrayResult = new ulong[n+1];
+ulong groups = 0;
+
+for(ulong count = 1; count<=n; count++)
+{
+    if(arrayResult[count]!=1)
+    {
+        groups++;
+        Console.Write($"\nГруппа {groups}: {count}");
+
+        arrayResult[count] = 1;
+        for(ulong i = count; i<=n; i++)
+        {
+            if(i%count != 0 && arrayResult[i] != 1)
+            {
+                Console.Write($", {i}");
+                arrayResult[i] = 1;
+            }
+        }
+    }
+}
+
+Console.WriteLine("\nВсего для числа " + n + " найдено " + groups + " групп чисел.\n");
+
+ulong Input(string output)
+{
+    Console.Write(output);
+    ulong result = 0;
+    while(result < 1)
+    {
+        result = Convert.ToUInt64(Console.ReadLine());
+        if(result < 1 )
+        {
+            Console.Write("Введите верное значение (оно должно быть больше 0): ");
+        }
+    }
+    return result;
+}
---
+++ GB74/Program.cs
@@ -0,0 +1,65 @@
+﻿
+Console.Clear();
+
+int friends = Input("Сколько друзей идут по барам: ");
+int maxBars = Input("Сколько пабов планируют посетить: ");
+double countPint = InputD("Сколько пинт выпьют в каждом баре: ");
+int minTimeDistance = Input("Сколько минимум времени идти до каждого бара: ");
+int maxTimeDistance = Input("Сколько максимум времени идти до каждого бара: ");
+int drinkTime = Input($"За сколько минут каждый друг выпивает пинту пенного: ");;
+
+double[] maxDrank = new double [friends];
+for(int i=0; i<friends; i++ )
+{
+    maxDrank[i] = InputD($"Сколько максимум литров может выпить {i+1}-й друг: ");
+}
+
+double[] countDrink = new double[friends];
+double pinta = 0.568;
+double drinksInMinute = pinta/drinkTime;
+
+int[] countBar = new int[friends];
+int[] countDrinkTime = new int[friends];
+
+int friend = 0;
+for(friend = 0; friend<friends; friend++)
+{
+    while(countDrink[friend] <= maxDrank[friend] && countBar[friend] < maxBars)
+    {
+        for(int i = 0; i<countPint*drinkTime; i++)
+        {
+            countDrink[friend]+= drinksInMinute;
+            countDrinkTime[friend]++;
+            if(countDrink[friend] > maxDrank[friend] )
+            {
+                break;
+            }
+        }
+        countBar[friend]++;
+    }
+
+    string barEnd = "бара";
+    if(countBar[friend]>4)
+    {
+        barEnd = "баров";
+    }
+
+    Console.Write($"\n{friend+1}-й друг пройдёт {countBar[friend]} {barEnd}. На выпивку он потратит {countDrinkTime[friend]} минут. ");
+
+    int minFullTime = countDrinkTime[friend] + minTimeDistance*(countBar[friend]-1);
+    int maxFullTime = countDrinkTime[friend] + maxTimeDistance*(countBar[friend]-1);
+    Console.WriteLine($"На хождение по барам и выпивку {friend+1}-й друг потратит от {minFullTime} до {maxFullTime} минут.");
+}
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double InputD(string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+}
